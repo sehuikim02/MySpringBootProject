dfd2099a47575475b1f59a9ee3f71daf4621869b
sehuikim02 <lulla1613@gmail.com>
2025-08-12 15:36:23 +0900

    Runner add

--- src/main/java/com/rookies4/myspringboot/MySpringBoot3ProjectApplication.java
+++ src/main/java/com/rookies4/myspringboot/MySpringBoot3ProjectApplication.java
@@ -1,6 +1,7 @@
 package com.rookies4.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -14,8 +15,11 @@ public class MySpringBoot3ProjectApplication {
 	public static void main(String[] args) {
 //        SpringApplication.run(MySpringBoot3ProjectApplication.class, args);
 
-        new SpringApplication(MySpringBoot3ProjectApplication.class);
-	}
+        SpringApplication application = new SpringApplication(MySpringBoot3ProjectApplication.class);
+        // application 타입을 변경하기
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.run(args);
+    }
 
     @Bean
     public String hello() {
