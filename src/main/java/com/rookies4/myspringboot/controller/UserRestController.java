5f932abedaa800fc322474c096e899fe6f74adc5
sehuikim02 <lulla1613@gmail.com>
2025-08-19 15:53:44 +0900

    UserInfo entity

--- src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -2,48 +2,50 @@ package com.rookies4.myspringboot.controller;
 
 import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.exception.BusinessException;
+import com.rookies4.myspringboot.repository.CustomerRepository;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController // @Controller + @ResponseBody
-@RequiredArgsConstructor    // lombok에서 final로 선언된 변수들을 초기화 하는 생성자를 자동으로 만들어줌
+@RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserRestController {
-    // @AutoWired   // Setter Injection
     private final UserRepository userRepository;
 
-    // Constructor Injection
+    //Constructor Injection
 //    public UserRestController(UserRepository userRepository) {
-//        System.out.println("생성자 Injection " + userRepository.getClass().getName());
+//        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
 
-    // 1. 등록
+    //등록
     @PostMapping
-    public UserEntity create(@RequestBody UserEntity user) {
+    public UserEntity create(@RequestBody UserEntity user){
         return userRepository.save(user);
     }
 
-    // 2. 조회
+    //전체목록 조회
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<UserEntity> getUsers() {
         return userRepository.findAll();
     }
-
-    // 2-1. ID로 조회
-    @GetMapping("/{id}")    // /api/users/1, /api/users/2
-    public UserEntity getUser(@PathVariable Long id) {
+    //ID로 조회
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public UserEntity getUser(@PathVariable Long id){
         UserEntity existUser = getExistUser(id);
         return existUser;
     }
-
-    // 2-2. Email로 조회하고, 수정
+    //Email로 조회하고, 수정하기
     @PatchMapping("/{email}/")
     public UserEntity updateUser(@PathVariable String email, @RequestBody UserEntity userDetail){
         UserEntity existUser = userRepository.findByEmail(email) //Optional<UserEntity>
@@ -54,22 +56,26 @@ public class UserRestController {
         UserEntity updateUser = userRepository.save(existUser);
         return updateUser;
     }
-
-    // 2-3. 삭제하기
+    //삭제하기
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
         UserEntity existUser = getExistUser(id);
-
-        // DB에서 삭제 요청
+        //DB에 삭제요청
         userRepository.delete(existUser);
         return ResponseEntity.ok("User가 삭제 되었습니다.");
     }
 
     private UserEntity getExistUser(Long id) {
         Optional<UserEntity> optionalUser = userRepository.findById(id);
-        // orElseThrow(Supplier) Supplier의 추상메소드 T get()
+        //orElseThrow(Supplier) Supplier의 추상메서드 T get()
         UserEntity existUser = optionalUser
                 .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
         return existUser;
+    }
+
+    //인증 없이 접근 가능한 메서드
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
     }
 }
---
+++ src/main/java/com/rookies4/myspringboot/security/models/UserInfo.java
@@ -0,0 +1,22 @@
+package com.rookies4.myspringboot.security.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    private String roles;
+}
